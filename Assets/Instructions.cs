8bae8c837669b63228272f62b873a016c81d99e1
drfofoffle <20sdeguara@slvusd.org>
2017-03-01 18:04:47 -0800

    Version 0.3
    Added instructions menu, more bug fixes. Added 3/1/17

---
+++ Assets/Instructions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Instructions : MonoBehaviour
+{
+    public Text txt;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void HitByRay()
+    {
+        txt.enabled = !txt.enabled;
+    }
+}
---
+++ Assets/InstructionsPop.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class InstructionsPop : MonoBehaviour {
+    public GameObject playerBro;
+    public GameObject CowDude;
+    Vector3 destination = new Vector3(90, 0, 0);
+    private bool falling;
+    public GameObject gun;
+
+    public GameObject Civillian;
+    public float deathRotaion;                
+    public float roationSmoothness;
+    public float appearenceScore = 0;
+    public float zRot;
+                         
+    private bool doDestroyLogic = true;
+    private float choice;
+    private float respawnModifier;               
+    private float respawnTime;
+    private float respawnModiferHome;
+    private float scoreHome;
+    public AudioClip Pop;
+    public AudioClip Bell;
+    private AudioSource SoundSource;
+    private bool soundTrigger = false;
+    public bool isStarter = false;
+    // Use this for initialization
+    void Start () {
+     
+        respawnModifier = 5;
+        respawnModiferHome = GameControl.respawnFloat;
+        zRot = this.transform.rotation.eulerAngles.z;
+        falling = false;
+        SoundSource = GetComponent<AudioSource>();
+        ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {         
+        if (GameControl.Score >= appearenceScore)
+        {
+            if (player.shot)
+            {
+                falling = true;
+            }
+            
+            
+            respawnModiferHome -= Time.deltaTime;
+            playerBro = GameObject.Find("Camera (eye)");
+            if (respawnModiferHome < 0)
+            {
+                    Vector3 targetPostition = new Vector3(playerBro.transform.position.x, this.transform.position.y, playerBro.transform.position.z);
+                    transform.LookAt(targetPostition);
+            }
+        }
+    }
+}
